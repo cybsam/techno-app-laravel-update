2fbb4f163b2a46e76eaef2f4d88f8d6cd0e99d26
SHUVO HALDER <42088593+cybsam@users.noreply.github.com>
2024-11-07 14:40:25 +0600

    fix: update dashboard team members blade layout

--- resources/views/dashboard/about-us/our-team/index.blade.php
+++ resources/views/dashboard/about-us/our-team/index.blade.php
@@ -15,123 +15,110 @@
 
 
     <hr>
-    <div class="card">
-        <div class="card-header">
-            <div class="text-danger">
-                @if (Session::get('validerr'))
-                <div class="alert alert-danger bg-danger text-light border-0 alert-dismissible fade show" role="alert">
-                    {{ Session::get('validerr') }}
-                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="alert"
-                        aria-label="Close"></button>
-                </div>
-                
-                @endif
-            </div>
-            <div class="text-info">
-                @if (Session::get('succ'))
-                    <div class="alert alert-primary bg-primary text-light border-0 alert-dismissible fade show"
-                        role="alert">
-                        {{ Session::get('succ') }}
-                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="alert"
-                            aria-label="Close"></button>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="text-danger">
+                            @if (Session::get('validerr'))
+                            <div class="alert alert-danger bg-danger text-light border-0 alert-dismissible fade show" role="alert">
+                                {{ Session::get('validerr') }}
+                                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="alert"
+                                    aria-label="Close"></button>
+                            </div>
+                            
+                            @endif
+                        </div>
+                        <div class="text-info">
+                            @if (Session::get('succ'))
+                                <div class="alert alert-primary bg-primary text-light border-0 alert-dismissible fade show"
+                                    role="alert">
+                                    {{ Session::get('succ') }}
+                                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="alert"
+                                        aria-label="Close"></button>
+                                </div>
+                                
+                            @endif
+                        </div>
+                        <div class="">
+                            @if ($errors->all())
+                                <span class="text-danger">
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </span>
+                            @endif
+                        </div>
+                        <div class="float-right">
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addteammember">+
+                                Member</button>
+                        </div>
                     </div>
+                    <div class="card-body">
+                        <table class="table table-hover datatable table-sm">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Image</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Degination</th>
+                                    <th scope="col">Department</th>
+                                    <th scope="col">Mobile</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Status</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
                     
-                @endif
-            </div>
-            <div class="">
-                @if ($errors->all())
-                    <span class="text-danger">
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </span>
-                @endif
-            </div>
-            <div class="float-right">
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addteammember">+
-                    Member</button>
+                                @foreach ($listMember as $key => $listMember)
+                                    <tr class="table-light">
+                                        <th scope="row"><a href="#">#{{ $listMember->id }}</a></td>
+                                        <td><img src="{{ asset('public/image/about-us/our-team') }}/{{ $listMember->image }}" height="35px"
+                                                width="35px" alt=""></td>
+                                        <td>{{ $listMember->name }}</td>
+                                        <td>{{ Str::limit($listMember->degination, 15) }}</td>
+                                        <td>{{ Str::limit($listMember->department, 15) }}</td>
+                                        <td>{{ Str::limit($listMember->mobile, 11) }}</td>
+                                        <td>{{ Str::limit($listMember->email, 10) }}</td>
+                                        <td>
+                                            <button class="badge {{ $listMember->is_active == 1 ? 'bg-warning':'bg-info'; }}" type="submit">
+                                                @if ($listMember->is_active == 1)
+                                                    <a href="{{ route('SupUser.OurTeamDeactive',['team_id' => $listMember->id]) }}" onclick="alert('Deactivated...');" class="text-white">deactive</a>
+                                                @else
+                                                    <a href="{{ route('SupUser.OurTeamActive',['team_id' => $listMember->id]) }}" onclick="alert('Activate...');" class="text-white">active</a>
+                                                @endif
+                                            </button>
+                                        </td>
+                                        <td>
+                                            <div class="btn-group" role="group">
+                    
+                                                <a href="{{ route('SupUser.OurTeamUpdate', ['team_id' => $listMember->id]) }}"
+                                                    class="btn btn-warning">
+                                                    <i class="bi bi-pencil-square" aria-hidden="true"></i>
+                                                </a>
+                                                <a href="{{ route('SupUser.OurTeamDelete', ['team_id' => $listMember->id]) }}"
+                                                    onclick="alert('Delete This Memeber');" class="btn btn-danger"><i class="bi bi-trash"
+                                                        aria-hidden="true"></i></a>
+                    
+                    
+                                            </div>
+                    
+                    
+                                        </td>
+                                    </tr>
+                                @endforeach
+                    
+                            </tbody>
+                            
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-
-    <table class="table table-hover datatable table-sm">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Image</th>
-                <th scope="col">Name</th>
-                <th scope="col">Degination</th>
-                <th scope="col">Department</th>
-                <th scope="col">Mobile</th>
-                <th scope="col">Email</th>
-                <th scope="col">Status</th>
-                <th scope="col">Action</th>
-            </tr>
-        </thead>
-        <tbody>
-
-            @foreach ($listMember as $key => $listMember)
-                <tr class="table-light">
-                    <th scope="row"><a href="#">#{{ $listMember->id }}</a></td>
-                    <td><img src="{{ asset('public/image/about-us/our-team') }}/{{ $listMember->image }}" height="35px"
-                            width="35px" alt=""></td>
-                    <td>{{ $listMember->name }}</td>
-                    <td>{{ Str::limit($listMember->degination, 15) }}</td>
-                    <td>{{ Str::limit($listMember->department, 15) }}</td>
-                    <td>{{ $listMember->mobile }}</td>
-                    <td>{{ $listMember->email }}</td>
-                    <td>
-                        <button class="badge {{ $listMember->is_active == 1 ? 'bg-warning':'bg-info'; }}" type="submit">
-                            @if ($listMember->is_active == 1)
-                                <a href="{{ route('SupUser.OurTeamDeactive',['team_id' => $listMember->id]) }}" onclick="alert('Deactivated...');" class="text-white">deactive</a>
-                            @else
-                                <a href="{{ route('SupUser.OurTeamActive',['team_id' => $listMember->id]) }}" onclick="alert('Activate...');" class="text-white">active</a>
-                            @endif
-                        </button>
-                    </td>
-                    <td>
-                        <div class="btn-group" role="group">
-
-                            <a href="{{ route('SupUser.OurTeamUpdate', ['team_id' => $listMember->id]) }}"
-                                class="btn btn-warning">
-                                <i class="bi bi-pencil-square" aria-hidden="true"></i>
-                            </a>
-                            <a href="{{ route('SupUser.OurTeamDelete', ['team_id' => $listMember->id]) }}"
-                                onclick="alert('Delete This Memeber');" class="btn btn-danger"><i class="bi bi-trash"
-                                    aria-hidden="true"></i></a>
-
-
-                        </div>
-
-
-                    </td>
-                </tr>
-            @endforeach
-
-        </tbody>
-        <tfoot>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Image</th>
-                <th scope="col">Name</th>
-                <th scope="col">Degination</th>
-                <th scope="col">Department</th>
-                <th scope="col">Mobile</th>
-                <th scope="col">Email</th>
-                <th scope="col">WhatsApp</th>
-                <th scope="col">Create at</th>
-                <th scope="col">Action</th>
-            </tr>
-        </tfoot>
-    </table>
-
-
-
-
-
-
-
-
+    
 
 
 

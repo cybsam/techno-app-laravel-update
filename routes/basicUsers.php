46fccf901c1b057feec389cf5eaf7ba82deee1e2
SHUVO HALDER <42088593+cybsam@users.noreply.github.com>
2024-11-10 02:46:05 +0600

    wr: adding user blogging route in users section

--- routes/basicUsers.php
+++ routes/basicUsers.php
@@ -9,4 +9,5 @@ use App\Http\Controllers\User\DashboardController;
 Route::prefix('en/User')->middleware(['auth:web','isUser','preventbackhistory','isVerifyEmail'])->group(function() {
     Route::get('Dashboard',[App\Http\Controllers\User\DashboardController::class, 'index'])->name('basicUser.dashboard');
     Route::post('contact/s',[App\Http\Controllers\User\DashboardController::class, 'contactStore'])->name('users.ContactStore');
+    Route::get('Dashboard/blog/post',[App\Http\Controllers\User\DashboardController::class, 'blogMethod'])->name('users.blogPost');
 });
